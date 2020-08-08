6dea329c5f619b85c2b1ec65d845ec444beffd01
gingkolane <gingkolane@gmail.com>
2020-08-08 11:16:06 -0400

    update MyConditions.js, to put in conditional rendering, for when myConditions is null

--- nihaoma-frontend/src/screens/InfoContainer/MyConditions.js
+++ nihaoma-frontend/src/screens/InfoContainer/MyConditions.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
-import PhoneframeForInfo from "../../components/PhoneframeForInfo"
+import React, { Component } from "react";
+import PhoneframeForInfo from "../../components/PhoneframeForInfo";
 
-import { connect } from 'react-redux'
-import { getSelectedCondition } from '../../reduxstore/condition.js'
-import { getTreatmentsOfSelectedCondition } from '../../reduxstore/treatment.js'
-import { getDoctorsOfSelectedCondition } from '../../reduxstore/doctor.js'
-import { getTrialsOfSelectedCondition } from '../../reduxstore/trial.js'
-
+import { connect } from "react-redux";
+import { getMyConditions } from "../../reduxstore/condition.js";
+import { getSelectedCondition } from "../../reduxstore/condition.js";
+// import { getTreatmentsOfSelectedCondition } from "../../reduxstore/treatment.js";
+// import { getDoctorsOfSelectedCondition } from "../../reduxstore/doctor.js";
+// import { getTrialsOfSelectedCondition } from "../../reduxstore/trial.js";
 
 class MyConditions extends Component {
-
   // defaultCondition = myConditions[0]
 
   // fetch(`http://localhost:3000/conditions/${defaultCondition.id}`)
   // .then(resp => resp.json())
-  // .then( data => { 
+  // .then( data => {
   //   this.props.getTreatmentsOfdefaultCondition(data.treatmentsOfdefaultCondition);
   //   this.props.getDoctorsOfdefaultCondition(data.doctorsOfdefaultCondition);
   //   this.props.getTrialsOfdefaultCondition(data.trialsOfdefaultCondition)
@@ -22,42 +21,70 @@ class MyConditions extends Component {
 
   handleClick = (e) => {
     //select clicked condition
-    const condition_id = parseInt(e.target.id)
-    const selectedCondition = this.props.myConditions.find(condition => condition.id === condition_id )
-    this.props.getSelectedCondition(selectedCondition) 
+    const condition_id = parseInt(e.target.id);
+    const selectedCondition = this.props.myConditions.find(
+      (condition) => condition.id === condition_id
+    );
+    this.props.getSelectedCondition(selectedCondition);
 
     //fetch treatments, trials and doctors data based on currentCondion id, and store the fetched data in redux store
     fetch(`http://localhost:3000/conditions/${selectedCondition.id}`)
-    .then(resp => resp.json())
-    .then( data => { 
-      this.props.getTreatmentsOfSelectedCondition(data.treatmentsOfCondition);
-      this.props.getDoctorsOfSelectedCondition(data.doctorsOfCondition);
-      this.props.getTrialsOfSelectedCondition(data.trialsOfCondition)
-      })
-    }
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.props.getTreatmentsOfSelectedCondition(data.treatmentsOfCondition);
+        this.props.getDoctorsOfSelectedCondition(data.doctorsOfCondition);
+        this.props.getTrialsOfSelectedCondition(data.trialsOfCondition);
+      });
+  };
 
   render() {
+    console.log(
+      "this is screencontent in myConditions this.props.myConditions",
+      this.props.myConditions
+    );
 
-    const screenContent = this.props.myConditions.map(condition => 
-      <h3 key={condition.id} id={condition.id} onClick={this.handleClick}> {condition.condition_name}</h3>)
-    
-    return <PhoneframeForInfo titleText = "My Conditions" screenContent={screenContent} />
+    let screenContent;
+
+    if (this.props.myConditions) {
+      screenContent = this.props.myConditions.map((condition) => (
+        <h3 key={condition.id} id={condition.id} onClick={this.handleClick}>
+          {" "}
+          {condition.condition_name}
+        </h3>
+      ));
+    } else {
+      screenContent = "No conditions yet.";
+    }
+
+    // const screenContent =
+    //   this.props.myConditions.map((condition) => (
+    //     <h3 key={condition.id} id={condition.id} onClick={this.handleClick}>
+    //       {" "}
+    //       {condition.condition_name}
+    //     </h3>
+    //   )) && "No conditions yet.";
+
+    return (
+      <PhoneframeForInfo
+        titleText="My Conditions"
+        screenContent={screenContent}
+      />
+    );
   }
-
 }
 
-const mapStateToProps = state => {
-  return { myConditions: state.condition.myConditions }
-}
+const mapStateToProps = (state) => {
+  return { myConditions: state.condition.myConditions };
+};
 
 const mapDispatchToProps = {
+  getMyConditions,
   getSelectedCondition,
-  getTreatmentsOfSelectedCondition,
-  getDoctorsOfSelectedCondition,
-  getTrialsOfSelectedCondition
-}
+  // getTreatmentsOfSelectedCondition,
+  // getDoctorsOfSelectedCondition,
+  // getTrialsOfSelectedCondition,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(MyConditions)
-
+export default connect(mapStateToProps, mapDispatchToProps)(MyConditions);
 
 // https://dev.to/markusclaus/fetching-data-from-an-api-using-reactredux-55ao
