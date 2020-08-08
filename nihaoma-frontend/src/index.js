d295f9f051fc028963c3a2161635ef41c6d6347e
gingkolane <gingkolane@gmail.com>
2020-08-08 11:18:37 -0400

    eslint cleanup

--- nihaoma-frontend/src/index.js
+++ nihaoma-frontend/src/index.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers } from 'redux';
-import { configureStore } from 'redux-starter-kit';
-import './stylesheets/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers } from "redux";
+import { configureStore } from "redux-starter-kit";
+import "./stylesheets/index.css";
+import App from "./App";
 //import reducers
-import conditionReducer from './reduxstore/condition'
-import userReducer from './reduxstore/user'
-import doctorReducer from './reduxstore/doctor'
-import treatmentReducer from './reduxstore/treatment'
-import trialReducer from './reduxstore/trial'
-import recordReducer from './reduxstore/record'
+import conditionReducer from "./reduxstore/condition";
+import userReducer from "./reduxstore/user";
+import doctorReducer from "./reduxstore/doctor";
+import treatmentReducer from "./reduxstore/treatment";
+import trialReducer from "./reduxstore/trial";
+import recordReducer from "./reduxstore/record";
 
 const rootReducer = combineReducers({
   condition: conditionReducer,
   user: userReducer,
   doctor: doctorReducer,
   treatment: treatmentReducer,
-  trial: trialReducer, 
-  record: recordReducer
+  trial: trialReducer,
+  record: recordReducer,
 });
 
 // create and configure store use redux-starter-kit
-const store = configureStore({ reducer: rootReducer }); 
+const store = configureStore({ reducer: rootReducer });
 
 // Provide store to App
-ReactDOM.render((
+ReactDOM.render(
   <Provider store={store}>
-      <App />
-  </Provider>
-), document.getElementById('root'));
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
