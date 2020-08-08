c249919c420eccabad0b8feb425e2cd52e5260bf
gingkolane <gingkolane@gmail.com>
2020-08-08 10:06:05 -0400

    refactor auth_controller, remove methods for treatments_for_my_conditions, myrecords_infull

--- nihaoma-api/app/controllers/auth_controller.rb
+++ nihaoma-api/app/controllers/auth_controller.rb
@@ -5,87 +5,94 @@ class AuthController < ApplicationController
 
   def create
     user = User.find_by(username: user_params[:username])
+
     # if username and password both match those in the database
     if user && user.authenticate(user_params[:password])
-      
-      treatments_for_my_conditions = user.conditions.map do |condition|
-        treatments_for_this_condition = condition.treatments
-        treatments_for_this_user = user.treatments 
-        treatments = user.treatments & condition.treatments
-        object = {
-            name: condition.condition_name,
-            treatments: treatments
-        }
-        object && 'No treatment yet'
-      end
-      
-      myrecords_infull = user.records.map do |record|
-        object = {
-          condition: record.condition,
-          doctor: record.doctor,
-          date_of_visit: record.date_of_visit,
-          treatments: record.treatments
-        }
-        object && 'No record yet'
-      end 
+
+      current_user = user
 
       render json: {
-        user: user, 
-        token: encode_token(user),
-        myConditions: user.conditions,
-        myRecords: user.records
-        # condition_with_treatment: treatments_for_my_conditions,
-        # myTreatments: user.treatments,
-        # myDoctors: user.doctors,
-        # myrecords_infull: user.myrecords_infull
+        token: encode_token(user), 
+        currentUser: current_user, 
+        myConditions: current_user.conditions,
+        myDoctors: current_user.doctors,
+        myRecords: current_user.records,
+        myTreatments: current_user.my_treatments, 
+        myGroups: current_user.groups
       }
+
     else
       render json: { errors: ["Wrong username or password bud. Sorry!"] }, status: :unprocessable_entity
     end
   end
 
   def persist
-    if token 
-      treatments_for_my_conditions = current_user.conditions.map do |condition|
-        treatments_for_this_condition = condition.treatments
-        treatments_for_this_user = current_user.treatments 
-        treatments = current_user.treatments & condition.treatments
-        
-        object = {
-          name: condition.condition_name,
-          treatments: treatments
-        }
 
-        object && 'No treatment yet'
-      end
-
-      myrecords_infull = current_user.records.map do |record|
-        object = {
-          condition: record.condition,
-          doctor: record.doctor,
-          date_of_visit: record.date_of_visit,
-          treatments: record.treatments
-        }
-        object && 'No record yet'
-      end 
+      if token 
+        current_user
 
       render json: { 
         currentUser: current_user, 
         myConditions: current_user.conditions,
-        myRecords: current_user.records ,
-        myTreatments: current_user.treatments,
+        myRecords: current_user.records,
+        myTreatments: current_user.my_treatments,
         # # is the name here correct? what is condition_with_treatment
         # condition_with_treatment: treatments_for_my_conditions,
         myDoctors: current_user.doctors,
-        myrecords_infull: myrecords_infull
+        myGroups: current_user.groups
+        # myrecords_infull: myrecords_infull
       }
     end
   end  
 
-
   private
   def user_params
-    params.permit(:username, :password)
+    params.permit(:username, :password, :auth)
   end
 
 end
+
+
+      # treatments_for_my_conditions = user.conditions.map do |condition|
+      #   treatments_for_this_condition = condition.treatments
+      #   treatments_for_this_user = user.treatments 
+      #   treatments = user.treatments & condition.treatments
+      #   object = {
+      #       name: condition.condition_name,
+      #       treatments: treatments
+      #   }
+      #   object && 'No treatment yet'
+      # end
+      
+      # myrecords_infull = user.records.map do |record|
+      #   object = {
+      #     condition: record.condition,
+      #     doctor: record.doctor,
+      #     date_of_visit: record.date_of_visit,
+      #     treatments: record.treatments
+      #   }
+      #   object && 'No record yet'
+      # end 
+
+            # treatments_for_my_conditions = current_user.conditions.map do |condition|
+      #   treatments_for_this_condition = condition.treatments
+      #   treatments_for_this_user = current_user.treatments 
+      #   treatments = current_user.treatments & condition.treatments
+        
+      #   object = {
+      #     name: condition.condition_name,
+      #     treatments: treatments
+      #   }
+
+      #   object && 'No treatment yet'
+      # end
+
+      # myrecords_infull = current_user.records.map do |record|
+      #   object = {
+      #     condition: record.condition,
+      #     doctor: record.doctor,
+      #     date_of_visit: record.date_of_visit,
+      #     treatments: record.treatments
+      #   }
+      #   object && 'No record yet'
+      # end 
