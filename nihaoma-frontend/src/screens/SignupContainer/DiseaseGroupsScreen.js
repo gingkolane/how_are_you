08c4f32579166c24c4a396ec2f7a329990c8acc8
gingkolane <gingkolane@gmail.com>
2020-08-08 11:21:13 -0400

    eslint cleanup

--- nihaoma-frontend/src/screens/SignupContainer/DiseaseGroupsScreen.js
+++ nihaoma-frontend/src/screens/SignupContainer/DiseaseGroupsScreen.js
@@ -1,69 +1,73 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 // import { createNewGroupsUser } from '../../reduxstore/groupsUser.js'
-import PhoneFrame from "../../components/Phoneframe"
-import '../../stylesheets/Phoneframe.css';
+import PhoneFrame from "../../components/Phoneframe";
+import "../../stylesheets/Phoneframe.css";
 
 class DiseaseGroupsScreen extends Component {
-  
   state = {
-    group_name: '',
+    groupName: "",
 
-    groups_user: {
-      group_id: null,
-      // user_id: this.props.current_user.id
-    }
-
-  }
+    groupsUser: {
+      groupId: null,
+      userId: this.props.currentUser.id,
+    },
+  };
 
   handleChange = (e) => {
-    this.setState({group_name: e.target.value})
-  }
+    this.setState({ groupName: e.target.value });
+  };
 
   handleOnClick = (e) => {
     //submit selected group to groups_users table to create new groupsUser, then send the new groupsUser to store
-    fetch("http://localhost:3000/groups_users",{ 
-        method: "post",
-        headers: {
-          "Content-Type":"application/json",
-          "Accepts":"application/json"},
-        body: JSON.stringify(this.state.groups_user)
-      })
-    .then(res => res.json())
-    .then(data => {
-      //dispatch new groups_user information to store
-      this.props.createNewGroupsUser({ groups_user: data.groups_user })
+    fetch("http://localhost:3000/groups_users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+      body: JSON.stringify(this.state.groups_user),
     })
+      .then((res) => res.json())
+      .then((data) => {
+        //dispatch new groups_user information to store
+        this.props.createNewGroupsUser({ groups_user: data.groups_user });
+      });
   };
 
   screenContent = (
     <div className="toc-buttons">
-     <h6>Disease groups you might be interested in:</h6>
-     
+      <h6>Disease groups you might be interested in:</h6>
+
       <button onClick={this.handleOnClick}> Stage I/II lung cancer </button>
-              
+
       <button onClick={this.handleOnClick}> Stage III lung cancer </button>
-             
+
       <button onClick={this.handleOnClick}> Stage IV lung cancer </button>
     </div>
-  )
+  );
 
   render() {
-    return <PhoneFrame titleText = { "Disease Groups" } 
-     screenContent={this.screenContent}/>
+    return (
+      <PhoneFrame
+        titleText={"Disease Groups"}
+        screenContent={this.screenContent}
+      />
+    );
   }
-
 }
- 
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    currentUser: state.user.currentUser
-  }
-}
+    currentUser: state.user.currentUser,
+  };
+};
 
 const mapDispatchToProps = {
   // createNewGroupsUser: createNewGroupsUser
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(DiseaseGroupsScreen)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DiseaseGroupsScreen);
--- nihaoma-frontend/src/screens/SignupContainer/OfficalGroupsScreen.js
+++ nihaoma-frontend/src/screens/SignupContainer/OfficalGroupsScreen.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
-import '../../stylesheets/Phoneframe.css';
-import PhoneFrame from "../../components/Phoneframe"
+import React, { Component } from "react";
+import "../../stylesheets/Phoneframe.css";
+import PhoneFrame from "../../components/Phoneframe";
 
 class OfficalGroupsScreen extends Component {
-  
   state = {
     group: {
       group_id: null,
       group_type: "official_group",
-      group_name: ''
-    }
-  }
+      group_name: "",
+    },
+  };
 
   handleChange = (e) => {
-    this.setState({ official_group: e.target.value })
-  }
+    this.setState({ official_group: e.target.value });
+  };
 
   screenContent = (
     <div className="toc-buttons">
       <h6>Official support groups you might be interested in:</h6>
-      <button onClick={this.handleOnClick}> Macmillan Cancer Support </button>        
-      <button onClick={this.handleOnClick}> Anticancer club - Apricot Grove </button>
+      <button onClick={this.handleOnClick}> Macmillan Cancer Support </button>
+      <button onClick={this.handleOnClick}>
+        {" "}
+        Anticancer club - Apricot Grove{" "}
+      </button>
     </div>
-  )
+  );
 
   render() {
-    return <PhoneFrame titleText = "Official Support Groups"  
-      screenContent={this.screenContent}/>
+    return (
+      <PhoneFrame
+        titleText="Official Support Groups"
+        screenContent={this.screenContent}
+      />
+    );
   }
-
 }
- 
+
 export default OfficalGroupsScreen;
