c6e7d62b2e7c30ffc3c3af4be44d00986a5b36dd
gingkolane <gingkolane@gmail.com>
2020-08-08 11:26:19 -0400

    prettier record.js

--- nihaoma-frontend/src/reduxstore/record.js
+++ nihaoma-frontend/src/reduxstore/record.js
@@ -1,25 +1,37 @@
-import { createSlice } from 'redux-starter-kit';
+import { createSlice } from "redux-starter-kit";
 
 const record = createSlice({
-  
-  slice: 'record',
-  
-  initialState: { 
-    myRecords: [], 
+  slice: "record",
+
+  initialState: {
+    myRecords: [],
     myRecordsInFull: [],
     selectedRecord: {},
-    recordsOfSelectedCondition: []
+    recordsOfSelectedCondition: [],
   },
 
   reducers: {
-    getMyRecords: (state, action) => { return {...state, myRecords: action.payload}},
-    getMyRecordsInFull: (state, action) => { return {...state, myRecordsInFull: action.payload}},
-    getSelectedRecord: (state, action) => { return {...state, selectedRecord: action.payload}},
-    getCreatedRecord: (state, action) => { return {...state, createdRecord: action.payload}}
+    getMyRecords: (state, action) => {
+      return { ...state, myRecords: action.payload };
+    },
+    getMyRecordsInFull: (state, action) => {
+      return { ...state, myRecordsInFull: action.payload };
+    },
+    getSelectedRecord: (state, action) => {
+      return { ...state, selectedRecord: action.payload };
+    },
+    getCreatedRecord: (state, action) => {
+      return { ...state, createdRecord: action.payload };
+    },
   },
-
 });
 
 const { actions, reducer } = record;
-export const { getMyRecords, getMyRecordsInFull, getCreatedRecord, getSelectedRecord} = actions;
-export default reducer
+
+export const {
+  getMyRecords,
+  getMyRecordsInFull,
+  getCreatedRecord,
+  getSelectedRecord,
+} = actions;
+export default reducer;
